d82c8c65f3e26f54dc6cfab29ef171d4380e80e4
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-07-28 01:23:06 +0530

    Lexicographical-Sorting-problem

--- ProgramFile/src/in/org/collections/ArraysLearning.java
+++ ProgramFile/src/in/org/collections/ArraysLearning.java
@@ -4,30 +4,34 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class ArraysLearning {
+    String name;
+    public ArraysLearning(String name) {
+        this.name = name;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {5,8,7,9,4,10,6,1};
-        for(int i: arr){
-            System.out.print(i+" ");
-        }
-        Arrays.sort(arr);
-        System.out.println();
-        System.out.println("After");
-        for(int i: arr){
-            System.out.print(i+" ");
-        }
-        System.out.println();
-        String[] names = {"Venkatesh","Saranya","Rangaraj","Bala"};
+
+//        ArraysLearning a1 = new ArraysLearning("Ram");
+//        ArraysLearning a2 = new ArraysLearning("Sridhar");
+//        ArraysLearning a3 = new ArraysLearning("Jaga");
+//        ArraysLearning[] abc = {a1,a2,a3};
+////        Arrays.sort(abc);
+//        for(int i=0;i<abc.length;i++){
+//            System.out.println(abc[i]);
+//        }
+        String[] names = {"Venkatesh","Saranya","Rangaraj","Bala","Venki","Shradha"};
+        System.out.println("Before Sorting...");
         for(String name : names){
             System.out.print(name+" ");
         }
-        System.out.println();
-        System.out.println("After");
-        Arrays.sort(names);
-        for(String name : names){
-            System.out.print(name+" ");
-        }
-        System.out.println();
-        Comparator comp = new ComparatorDemo();
+//        System.out.println();
+//        System.out.println("After");
+//        Arrays.sort(names);
+//        for(String name : names){
+//            System.out.print(name+" ");
+//        }
+//        System.out.println();
+        Comparator<String> comp = new ComparatorDemo();
         Arrays.sort(names,comp);
         System.out.println("After Sort");
         for(String name : names){
--- ProgramFile/src/in/org/collections/ComparatorDemo.java
+++ ProgramFile/src/in/org/collections/ComparatorDemo.java
@@ -7,12 +7,21 @@ public class ComparatorDemo implements Comparator {
     public int compare(Object o1, Object o2) {
         String s1 = (String) o1;
         String s2 = o2.toString();
-        if(s1.length() > s2.length()){
-            return -1;
-        } else if (s1.length() < s2.length()) {
-            return 1;
-        }else {
-            return 0;
-        }
+        return s1.compareTo(s2);//--->Lexicographical Sorting..
+//        int result = s1.compareTo(s2);
+//        if(result>0){
+//            return +1;
+//        } else if (result < 0) {
+//            return -1;
+//        }else {
+//            return 0;
+//        }//---> use to find the order
+//        if(s1.length() < s2.length()){
+//            return -1;
+//        } else if (s1.length() > s2.length()) {
+//            return +1;
+//        }else {
+//            return 0;
+//        }//--->use to Find the Length of sort
     }
 }
