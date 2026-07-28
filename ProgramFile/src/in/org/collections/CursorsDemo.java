4b8330c566c9d30b8fc66d2451434d388a22d1f7
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-07-29 00:25:07 +0530

    BiDirectional-Cursor(ListIterator)

--- ProgramFile/src/in/org/collections/CursorsDemo.java
+++ ProgramFile/src/in/org/collections/CursorsDemo.java
@@ -3,6 +3,7 @@ package in.org.collections;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class CursorsDemo {
     public static void main(String[] args) {
@@ -12,12 +13,24 @@ public class CursorsDemo {
         ar.add("Bala");
         ar.add(5);
         System.out.println("Before"+ar);
-        Iterator iter = ar.iterator();
-        while (iter.hasNext()){
-            if (iter.next().equals(5)){
-                iter.remove();
+//        Iterator iter = ar.iterator();
+//        while (iter.hasNext()){
+//            if (iter.next().equals(5)){
+//                iter.remove();
+//            }
+//        }
+//        System.out.println("After"+ar);//Single Directional Cursor Method hasNext() , next() ,
+//        add() , remove()
+        System.out.println();
+        ListIterator lt = ar.listIterator();
+        lt.add("Ranga");
+        while(lt.hasNext()){
+            if (lt.next().equals(5)){
+                lt.set(101);
             }
         }
-        System.out.println("After"+ar);//Single Directional Cursor (hasNext())
+        System.out.println("After ListIterator"+ar);
+        /* Bidirectional cursor , can move forward and backwards
+        *  metohs are  - hasNext() , next() , hasPrevious() ,previous() ,add() , remove() set() */
     }
 }
