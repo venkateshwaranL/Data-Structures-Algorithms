50dc4b597dd1d5d97355667b513ab5117442c657
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-07-29 23:30:44 +0530

    map-interface

---
+++ ProgramFile/src/in/org/collections/MapDemo.java
@@ -0,0 +1,25 @@
+package in.org.collections;
+
+import java.util.*;
+
+public class MapDemo {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put(101,"Venkatesh");
+        map.put(102,"Saanya");
+        map.put(103,"Rangaraj");
+        map.put(105,"Bala");
+        map.put(104,"Ram");
+        System.out.println(map);
+        Map<Integer,String> mp = new LinkedHashMap<>();
+        mp.put(101,"Sri");
+        mp.put(104,"Jaga");
+        mp.put(103,"Siva");
+        System.out.println(mp);
+        Map<Integer,String> tm =new TreeMap<>();
+        tm.put(109,"Jeeva");
+        tm.put(104,"Sai");
+        tm.put(101,"Sandy");
+        System.out.println(tm);
+    }
+}
