e94b9eaa478473b435cf09504bd097f8a24d2bda
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-07-29 18:08:05 +0530

    three-typeof-set-interface

--- ProgramFile/src/in/org/collections/SetDemo.java
+++ ProgramFile/src/in/org/collections/SetDemo.java
@@ -9,18 +9,23 @@ public class SetDemo {
     public static void main(String[] args) {
         Set hs = new HashSet();
         Set ls = new LinkedHashSet();
-        Set ts = new TreeSet();
+        TreeSet ts = new TreeSet();
         hs.add("Venkateshwaran");
-        hs.add("Rangaraj");
+        hs.add(55);
         hs.add("Bhuveneswari");
+        hs.add(null);
+        hs.add(null);
         ls.add("viky");
-        ls.add("Karthich");
+        ls.add("10");
         ls.add("Bala");
         ls.add("Raj");
         ts.add("Saranya");
         ts.add("Ram");
         ts.add("Nizer");
-        ts.add("Viswanth");
+        ts.add("0"+9);
+        ts.add("0"+1);
+        ts.add("1"+55);
+//        ts.add(10);//.ClassCastException
         for(Object name : hs){
             System.out.print(name+" ");
         }
