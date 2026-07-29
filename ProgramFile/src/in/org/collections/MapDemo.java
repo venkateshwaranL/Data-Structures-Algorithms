a9444034df87c3e7a1f39c5b73acd5c947173166
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-07-30 00:44:10 +0530

    three-typesOf-map-interface

--- ProgramFile/src/in/org/collections/MapDemo.java
+++ ProgramFile/src/in/org/collections/MapDemo.java
@@ -11,15 +11,20 @@ public class MapDemo {
         map.put(105,"Bala");
         map.put(104,"Ram");
         System.out.println(map);
-        Map<Integer,String> mp = new LinkedHashMap<>();
+        Map mp = new LinkedHashMap<>();
         mp.put(101,"Sri");
         mp.put(104,"Jaga");
         mp.put(103,"Siva");
         System.out.println(mp);
-        Map<Integer,String> tm =new TreeMap<>();
+        Map tm =new TreeMap<>();
         tm.put(109,"Jeeva");
         tm.put(104,"Sai");
         tm.put(101,"Sandy");
         System.out.println(tm);
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+        Set setview = tm.keySet();
+        System.out.println(setview);
+        Collection valuesview = tm.values();
+        System.out.println(valuesview);
     }
 }
